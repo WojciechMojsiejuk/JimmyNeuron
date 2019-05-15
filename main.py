aed5cbf55e9f00fb658a2fb47517b318d3432d01
Wojciech Mojsiejuk <wojciechmojsiejuk@macbook-pro-wojciech.home>
2019-05-15 10:16:16 +0200

    second commit

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-from kivy.app import App
-from kivy.uix.label import Label
+__author__      = "Ricardo Ribeiro"
+__credits__     = ["Ricardo Ribeiro"]
+__license__     = "MIT"
+__version__     = "0.0"
+__maintainer__  = "Ricardo Ribeiro"
+__email__       = "ricardojvr@gmail.com"
+__status__      = "Development"
 
 
-class MyApp(App):
+from pyforms.basewidget import BaseWidget
+from pyforms.controls 	import ControlText
+from pyforms.controls import ControlButton
 
-    def build(self):
-        return Label(text='Hello world')
+import pyforms
 
 
-if __name__ == '__main__':
-    MyApp().run()
+
+class SimpleExample4(BaseWidget):
+
+	def __init__(self):
+		super(SimpleExample4,self).__init__('Simple example 4')
+
+		#Definition of the forms fields
+		self._firstname 	= ControlText('First name', 'Default value')
+		self._middlename 	= ControlText('Middle name')
+		self._lastname 		= ControlText('Lastname name')
+		self._fullname 		= ControlText('Full name')
+		self._button 		= ControlButton('Press this button')
+
+		#Define the organization of the forms
+		self.formset = [ {
+						  	'Tab1':['_firstname','||','_middlename','||','_lastname'],
+						  	'Tab2': ['_fullname']
+						  },
+						  '=',(' ','_button', ' ') ]
+		#Use dictionaries for tabs
+		#Use the sign '=' for a vertical splitter
+		#Use the signs '||' for a horizontal splitter
+
+		#Define the button action
+		self._button.value = self.__buttonAction
+
+
+	def __buttonAction(self):
+		"""Button action event"""
+		self._fullname.value = self._firstname.value +" "+ self._middlename.value + \
+		" "+ self._lastname.value
+
+
+
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+
+#Execute the application
+if __name__ == "__main__":	 pyforms.start_app( SimpleExample4 )
